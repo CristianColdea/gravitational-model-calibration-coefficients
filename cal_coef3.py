4aec2bc549fa5e11a9b3a782dd2f6c98629081ea
CristianColdea <rdt333@gmail.com>
2024-05-14 16:32:09 +0300

    script updates

--- cal_coef3.py
+++ cal_coef3.py
@@ -363,7 +363,7 @@ gvalsr = Gravitmod.gravmod_init(travs, ffs, k_ij0)
 
 gvalsr_m = [gvalsr[i:i + 3] for i in range(0, len(gvalsr), 3)]
 
-Gravitmod.iter_adj_in(travs, gvalsr_m)
+# Gravitmod.iter_adj_in(travs, gvalsr_m)
 # print(gvalsr_m)
 
 # print(gvalsradj)
